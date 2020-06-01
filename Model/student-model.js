7e56ca41f4ed41fcbc71cd09387768268a0862d2
Riyas Ahamed <riyasahamed@10decoders.in>
2020-06-01 20:34:38 +0530

    Added JOI for validation

--- Model/student-model.js
+++ Model/student-model.js
@@ -1,10 +1,24 @@
 var mongoose    =   require("mongoose");
-const studentSchema =   new mongoose.Schema({
-    "user_Id" : Number,
-    "StudentName" : String,
-    "StudentRegno" : Number,
-    "StudentDept" : String
+var Joi = require('@hapi/joi');
+const joigoose = require('joigoose')(mongoose)
+// const studentSchema =   new mongoose.Schema({
+//     "user_Id" : Number,
+//     "StudentName" : String,
+//     "StudentRegno" : Number,
+//     "StudentDept" : String
+// },{
+//     unique:true
+// })
+const joiSchema = Joi.object({
+    user_Id : Joi.string().regex(/[0-9]+/).length(4).required(),
+    StudentName: Joi.string().min(6).max(30).required(),
+    StudentRegno: Joi.string().regex(/[0-9]+/).length(5).required(),
+    StudentDept: Joi.string().min(3).max(6).required(),
 })
 
+const studentSchema = new mongoose.Schema(joigoose.convert(joiSchema))
+
+studentSchema.index({user_Id: 1}, {unique: true});
+studentSchema.index({StudentRegno: 1}, {unique: true});
 // create model if not exists.
 module.exports = mongoose.model('Student',studentSchema);
--- Model/user.js
+++ Model/user.js
@@ -1,14 +1,44 @@
 
 var mongoose    =   require("mongoose");
-const userSchema =   new mongoose.Schema({
-    "user_Id" : Number,
-    "StudentName" : String,
-    "StudentDept" : String,
-    "Email" : String,
-    "Password": String,
-    "Token":String
+var Joi = require('@hapi/joi');
+// const userSchema =   new mongoose.Schema({
+    
+//     "user_Id" : Number,
+//     "StudentName" : String,
+//     "StudentDept" : String,
+//     "Email" : String,
+//     "Password": String,
+//     "Token":String
+// })
+// userSchema.methods.schemaValidate = function(obj) {
+// 	const schema = {
+//         user_Id : Joi.types.String().min(4).max(4).required(),
+//         StudentName: Joi.types.String().min(6).max(30).required(),
+//         StudentDept: Joi.types.String().min(3).max(6).required(),
+//         Email: Joi.types.String().email().required(),
+//         Password: Joi.types.String().min(6).max(30).regex(/[a-zA-Z0-9]{3,30}/).required(),
+//         Token: Joi.types.String().required(),
+// 		created: Joi.types.Date(),
+// 	}
+// 	return Joi.validate(obj, schema);express-validator schemaexpress-validator schema
+// }
+
+const joigoose = require('joigoose')(mongoose)
+
+const joiSchema = Joi.object({
+    user_Id : Joi.string().regex(/[0-9]+/).length(4).required(),
+    StudentName: Joi.string().min(6).max(30).required(),
+    StudentDept: Joi.string().min(3).max(6).required(),
+    Email: Joi.string().email().required(),
+    Password: Joi.string().min(6).max(30).regex(/[a-zA-Z0-9]{3,30}/).required(),
+    Token: Joi.string(),
 })
 
+const userSchema = new mongoose.Schema(joigoose.convert(joiSchema))
+
+userSchema.index({user_Id: 1}, {unique: true});
+userSchema.index({Email: 1}, {unique: true});
+userSchema.index({Password: 1}, {unique: true});
 // create model if not exists.
 module.exports = mongoose.model('User',userSchema);
 
